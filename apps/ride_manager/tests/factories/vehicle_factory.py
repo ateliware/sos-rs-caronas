d63f897da2a6d89b7ab1f1cc0751c877673f6ace
Gabriel Silveira <gabrielsilveirapro@gmail.com>
2024-05-08 11:19:41 -0300

    fix vehicle tests

--- apps/ride_manager/tests/factories/vehicle_factory.py
+++ apps/ride_manager/tests/factories/vehicle_factory.py
@@ -1,10 +1,10 @@
 import factory
 from faker import Faker
 
-from apps.ride_manager.models.vehicle import Vehicle
+from apps.ride_manager.models import Vehicle
+from apps.ride_manager.tests.factories.person_factory import PersonFactory
 
 fake = Faker("pt_BR")
-
 
 class VehicleFactory(factory.django.DjangoModelFactory):
     model = fake.name()
@@ -13,6 +13,7 @@ class VehicleFactory(factory.django.DjangoModelFactory):
     plate_picture = factory.django.ImageField(filename='plate_picture.jpg')
     vehicle_picture = factory.django.ImageField(filename='vehicle_picture.jpg')
     is_verified = fake.boolean()
+    person = factory.SubFactory(PersonFactory)
 
     class Meta:
         model = Vehicle
