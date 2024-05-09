3835deb94b67a49eb78c9c578aa085e501c90aed
Victor Billy da Silva <victor.silva@ateliware.com>
2024-05-09 16:40:42 -0300

    create endpoint and tests

--- apps/ride_manager/apis/vehicle_viewset.py
+++ apps/ride_manager/apis/vehicle_viewset.py
@@ -1,10 +1,8 @@
-from apps.core.apis.generic_viewset_user_validator import (
-    GenericViewSetUserValidator,
-)
+from apps.core.apis.generic_viewset_user_validator import GenericUserViewSet
 from apps.ride_manager.models.vehicle import Vehicle
 from apps.ride_manager.serializers.vehicle_serializer import VehicleSerializer
 
 
-class VehicleViewset(GenericViewSetUserValidator):
+class VehicleViewset(GenericUserViewSet):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
--- apps/ride_manager/tests/factories/vehicle_factory.py
+++ apps/ride_manager/tests/factories/vehicle_factory.py
@@ -1,8 +1,14 @@
+import base64
+
 import factory
+from django.core.files.base import ContentFile
 from faker import Faker
 
 from apps.ride_manager.models import Vehicle
 from apps.ride_manager.tests.factories.person_factory import PersonFactory
+from apps.ride_manager.tests.factories.valid_base64_image_factory import (
+    valid_base64_image,
+)
 
 fake = Faker("pt_BR")
 
@@ -22,14 +28,13 @@ class VehicleFactory(factory.django.DjangoModelFactory):
             "model": fake.name(),
             "color": fake.color_name(),
             "plate": fake.license_plate(),
-            "plate_picture": factory.django.ImageField(
-                filename="plate_picture.jpg"
+            "plate_picture": ContentFile(
+                base64.b64decode(valid_base64_image()), name="plate_picture.jpg"
             ),
-            "vehicle_picture": factory.django.ImageField(
-                filename="vehicle_picture.jpg"
+            "vehicle_picture": ContentFile(
+                base64.b64decode(valid_base64_image()), name="plate_picture.jpg"
             ),
             "is_verified": fake.boolean(),
-            "person": PersonFactory.person_data(),
         }
 
     class Meta:
