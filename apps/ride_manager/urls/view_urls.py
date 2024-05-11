f28d7096614e49558caec141a8f51f11c07efc08
Victor Billy da Silva <victor.silva@ateliware.com>
2024-05-11 02:56:07 -0300

    flow of add vehicle for a person

--- apps/ride_manager/urls/view_urls.py
+++ apps/ride_manager/urls/view_urls.py
@@ -5,7 +5,7 @@ from apps.ride_manager.views.login import login_view
 from apps.ride_manager.views.logout import logout_view
 from apps.ride_manager.views.person import PersonView
 from apps.ride_manager.views.ride import create_ride
-from apps.ride_manager.views.vehicle import create_vehicle
+from apps.ride_manager.views.vehicle import create_vehicle, created_with_success
 
 urlpatterns = [
     path("login/", login_view, name="login"),
@@ -14,4 +14,5 @@ urlpatterns = [
     path("person/create/", PersonView.create, name="register"),
     path("ride/create/", create_ride, name="create_ride"),
     path("vehicle/add/", create_vehicle, name="add_vehicle"),
+    path("vehicle/success/", created_with_success, name="success_vehicle_save"),
 ]
--- apps/ride_manager/views/vehicle.py
+++ apps/ride_manager/views/vehicle.py
@@ -10,18 +10,48 @@ def create_vehicle(request):
     if request.method == "POST":
         form = VehicleForm(request.POST, request.FILES)
 
+        print("\n\n-----------HERE\n\n")
         if form.is_valid():
-            vehicle = form.save(commit=False)
-            vehicle.person = get_person(request)
+            print("Form is valid.")
+            cnh_number = form.cleaned_data.get('cnh_number')
+            cnh_picture = request.FILES.get('cnh_picture')
 
-            vehicle.save()
-            return redirect(
-                "create_ride"
-            )  # Redirect to a success page or another view
+            person = get_person(request)
+            print("\n\n-----------HERE\n\n")
+            print(person)
+            if person:
+                print("Person found.")
+                person.cnh_number = cnh_number
+                if cnh_picture:  # Ensure a file was uploaded
+                    person.cnh_picture = cnh_picture
+                print("Person data updated.")
+                try:
+                    person.save()
+                except Exception as e:
+                    logging.error(f"Error saving person data: {e}")
+                    return render(request, "create_vehicle.html", {"form": form, "error": "Erro ao salvar dados do usuário."})
+                print("Person data saved.")
+                vehicle = form.save(commit=False)
+                print("Vehicle data saved 1.")
+                vehicle.person = person
+                try:
+                    vehicle.save()
+                    print("Vehicle data saved2.")
+                except Exception as e:
+                    logging.error(f"Error saving vehicle data: {e}")
+                    return render(request, "create_vehicle.html", {"form": form, "error": "Erro ao salvar dados do veículo."})
+
+                return redirect("success_vehicle_save")
+        else:
+            logging.error("Invalid form data.")
+            print(form.errors)
+           
     else:
         form = VehicleForm()
     return render(request, "create_vehicle.html", {"form": form})
 
+def created_with_success(request):
+    return render(request, "created_with_success.html")
 
 def get_person(request) -> Person:
     person = None
