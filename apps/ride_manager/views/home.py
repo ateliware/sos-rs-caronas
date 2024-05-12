77bec78fc0220b414ea6297567b0150ca4116885
Victor Billy da Silva <victor.silva@ateliware.com>
2024-05-12 18:41:53 -0300

    layout adjusts

--- apps/ride_manager/views/home.py
+++ apps/ride_manager/views/home.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 @login_required(login_url="/login/")
 def home_view(request):
     return render(request, "home.html")
+
+def public_home(request):
+    return render(request, "public/home.html")
