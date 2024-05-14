3eb3ee0c84fe2c9f82469b7a2e9f02488f4f377d
Victor Billy da Silva <victor.silva@ateliware.com>
2024-05-14 13:42:02 -0300

    revert use_s3 django config

--- config/settings/static.py
+++ config/settings/static.py
@@ -1,7 +1,6 @@
 import os
 
 from .environments import BASE_DIR
-from decouple import config
 
 TEMPLATES = [
     {
@@ -19,32 +18,11 @@ TEMPLATES = [
     },
 ]
 
-USE_S3 = config("USE_S3", default=False, cast=bool)
 
-if USE_S3:
-    # aws settings
-    AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
-    AWS_DEFAULT_ACL = None
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-    # s3 static settings
-    STATIC_LOCATION = "static"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
-    # s3 public media settings
-    PUBLIC_MEDIA_LOCATION = "media"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    # s3 private media settings
-    PRIVATE_MEDIA_LOCATION = "private"
-    PRIVATE_FILE_STORAGE = "apps.core.storage_backends.PrivateMediaStorage"
-else:
-    STATIC_URL = "/static/"
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static", "css"),
-        os.path.join(BASE_DIR, "static", "img"),
-        os.path.join(BASE_DIR, "static", "html"),
-    ]
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static", "css"),
+    os.path.join(BASE_DIR, "static", "img"),
+    os.path.join(BASE_DIR, "static", "html"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
