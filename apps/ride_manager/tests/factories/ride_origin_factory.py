b5392c760cc9c057ce822dab10c41730213f1a5c
Victor Billy da Silva <victor.silva@ateliware.com>
2024-05-20 11:53:38 -0300

    add admin and admin tests

--- apps/ride_manager/tests/factories/ride_origin_factory.py
+++ apps/ride_manager/tests/factories/ride_origin_factory.py
@@ -1,20 +1,21 @@
 import factory
 from faker import Faker
 
+from apps.address_manager.tests.factories.city_factory import CityFactory
 from apps.ride_manager.models.ride_origin import RideOrigin
 
 fake = Faker("pt_BR")
 
 
 class RideOriginFactory(factory.django.DjangoModelFactory):
-    name = fake.city()
-    is_enabled = True
+    city = factory.SubFactory(CityFactory)
+    enabled = True
 
     @staticmethod
     def ride_origin_data() -> dict:
         return {
-            "name": fake.city(),
-            "is_enabled": True,
+            "city": factory.SubFactory(CityFactory),
+            "enabled": True,
         }
 
     class Meta:
